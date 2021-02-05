592544afb8e2c1fd4a831e3cdf8d2e17c93eb617
Roman Romanov <nicodim@mail.ru>
2021-02-05 13:15:07 +0300

    php8

--- classes/Comments/Comment.php
+++ classes/Comments/Comment.php
@@ -17,9 +17,10 @@ class Comment extends \Cetera\Material
         
 	public function getNickname()
 	{
-		if ($this->fields['nickname']) return $this->fields['nickname'];
-		if ($this->autor->name) return $this->autor->name;
-		if ($this->autor->login) return $this->autor->login;
+		if (isset($this->fields['nickname'])) return $this->fields['nickname'];
+		if ($this->autor && $this->autor->name) return $this->autor->name;
+		if ($this->autor && $this->autor->login) return $this->autor->login;
+        return null;
 	}
 	
 	public function getAvatar()
